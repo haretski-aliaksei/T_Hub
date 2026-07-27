94d97fb06d7f3eeca40e3516ec4cdb32afa36638
Aliaksei Haretski <haretski.aliaksei@gmail.com>
2026-07-27 20:48:01 +0200

    Add API base URL fixture

--- tests/api/products/test_products_basic.py
+++ tests/api/products/test_products_basic.py
@@ -3,8 +3,8 @@ import requests
 
 
 @pytest.mark.api
-def test_get_product_by_valid_id_returns_product_details():
-    response = requests.get("https://dummyjson.com/products/1", timeout=5)
+def test_get_product_by_valid_id_returns_product_details(base_url):
+    response = requests.get(f"{base_url}/products/1", timeout=5)
 
     assert response.status_code == 200
 
